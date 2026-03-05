a1adfffd626b21d1e212dc73da1860008103a83a
Fuqiao Xue <xfq@w3.org>
2026-03-05 22:58:03 +0800

    fix: update site URL and add W3C link in index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-// export default defineConfig({
-//   site: 'https://xfq.github.io',
-//   base: '/cjk-layout-lab',
-// })
-
 export default defineConfig({
-  site: "https://cjk-lab.xuefuqiao.com",
-});
+  site: 'https://xfq.github.io',
+  base: '/cjk-layout-lab',
+})
+
+// export default defineConfig({
+//   site: "https://cjk-lab.xuefuqiao.com",
+// });

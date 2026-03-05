cff1e254230f13d91d0adf9514c6fce72b8fe5b9
Fuqiao Xue <xfq@w3.org>
2026-03-05 22:44:43 +0800

    Try updating site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+// export default defineConfig({
+//   site: 'https://xfq.github.io',
+//   base: '/cjk-layout-lab',
+// })
+
 export default defineConfig({
-  site: 'https://xfq.github.io',
-  base: '/cjk-layout-lab',
-})
+  site: "https://cjk-lab.xuefuqiao.com",
+});
